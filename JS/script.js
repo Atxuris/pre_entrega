60d6106fce802cd664cf5791cdffe814ef3f311b
Atxuris <getatx@gmail.com>
2025-12-03 16:00:58 -0300

    Cambios en eltilos y textos descriptivos de los productos

--- JS/script.js
+++ JS/script.js
@@ -1,8 +1,3 @@
 console.log("Hola desde el Upside Down!");
 console.log("Amo Stranger Things!!! AAAAAHHHHHH!!!!");
 
-
-let person = prompt("¿Cómo te llamas?");
-alert("¡Hola " + person + "! Bienvenido al Upside Down.");
-
-document.getElementById("banner_text").innerHTML = "¡Prepárate para una aventura en el Upside Down, " + person + "!";
