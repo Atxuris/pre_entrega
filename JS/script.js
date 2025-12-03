934581afc72e0af0a86bf03f7c8728fe348da62c
Atxuris <getatx@gmail.com>
2025-12-03 09:40:12 -0300

    Modificaciones en style y pag principal

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+console.log("Hola desde el Upside Down!");
+console.log("Amo Stranger Things!!! AAAAAHHHHHH!!!!");
+
+
+let person = prompt("¿Cómo te llamas?");
+alert("¡Hola " + person + "! Bienvenido al Upside Down.");
+
+document.getElementById("banner_text").innerHTML = "¡Prepárate para una aventura en el Upside Down, " + person + "!";
